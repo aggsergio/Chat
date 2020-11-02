d9924bd8ac777c9f9846eba7e217b974ca32f7ec
Sergio <sergioguerra2499@gmail.com>
2020-11-02 03:01:46 -0600

    Tarea
    a

---
+++ cliente chat/cliente chat/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace cliente_chat
+{
+    public partial class Form1 : Form
+    {
+
+        static private NetworkStream stream;
+        static private StreamWriter streamw;
+        static private StreamReader streamr;
+        static private TcpClient client = new TcpClient();
+        static private string nombre = "Anon";
+        private delegate void DAddItem(String s);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            streamw.WriteLine(textBox2.Text);
+            streamw.Flush();
+            textBox2.Clear();
+        }
+
+        private void AddItem(String s)
+        {
+            listBox1.Items.Add(s);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            textBox1.Visible = false;
+            button1.Visible = false;
+            listBox1.Visible = true;
+            textBox2.Visible = true;
+            button2.Visible = true;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = true;
+            button3.Visible = true;
+            label2.Visible = true;
+            nombre = textBox1.Text;
+            label2.Text = "Bienvenido " + nombre;
+            Conectar();
+
+        }
+
+        void Conectar()
+        {
+            try
+            {
+                client.Connect("127.0.0.1", 8000);
+                if (client.Connected)
+                {
+                    Thread t = new Thread(Listen);
+
+                    stream = client.GetStream();
+                    streamw = new StreamWriter(stream);
+                    streamr = new StreamReader(stream);
+
+                    streamw.WriteLine(nombre);
+                    streamw.Flush();
+
+                    t.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Desconectado");
+                    Application.Exit();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Desconectado");
+                Application.Exit();
+            }
+        }
+
+        void Listen()
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    this.Invoke(new DAddItem(AddItem), streamr.ReadLine());
+
+                }
+                catch
+                {
+                    MessageBox.Show("Implosible conectar al servidor");
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ servidor chat/servidor chat/serverchat.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+namespace Servidor
+{
+    class Servidor_Chat
+    {
+        private TcpListener server;
+        private TcpClient client = new TcpClient();
+        private IPEndPoint ipendpoint = new IPEndPoint(IPAddress.Any, 8000);
+        private List<Connection> list = new List<Connection>();
+
+        Connection con;
+
+        private struct Connection
+        {
+            public NetworkStream stream;
+            public StreamWriter streamw;
+            public StreamReader streamr;
+            public string nombre;
+        }
+        public Servidor_Chat()
+        {
+            Inicio();
+        }
+
+        public void Inicio()
+        {
+            Console.WriteLine("Conectado");
+            server = new TcpListener(ipendpoint);
+            server.Start();
+            while (true)
+            {
+                client = server.AcceptTcpClient();
+
+                con = new Connection();
+                con.stream = client.GetStream();
+                con.streamr = new StreamReader(con.stream);
+                con.streamw = new StreamWriter(con.stream);
+
+                con.nombre = con.streamr.ReadLine();
+
+                list.Add(con);
+                Console.WriteLine(con.nombre + " ha entrado al chat.  Bienvenido de nuevo.");
+                Thread t = new Thread(Escuchar_conexion);
+                t.Start();
+            }
+        }
+        void Escuchar_conexion()
+        {
+            Connection hcon = con;
+            do
+            {
+                try
+                {
+                    string tmp = hcon.streamr.ReadLine();
+                    Console.WriteLine(hcon.nombre + ": " + tmp);
+                    foreach (Connection c in list)
+                    {
+                        try
+                        {
+                            c.streamw.WriteLine(hcon.nombre + ": " + tmp);
+                            c.streamw.Flush();
+                        }
+                        catch
+                        {
+                        }
+                    }
+                }
+                catch
+                {
+                    list.Remove(hcon);
+                    Console.WriteLine(con.nombre + " se ha desconectado del chat. Bye, bye!~");
+                    break;
+                }
+            } while (true);
+        }
+
+    }
+}
